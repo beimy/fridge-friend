6105b235b1448d18807d64236e52998f424cf4be
Beimy <arbm159357@gmail.com>
2022-07-21 16:22:42 -0400

    implemented react-router, cleaned up client folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink }  from '@apollo/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { setContext } from '@apollo/client/link/context'
+
+import Home from './pages/Home'
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -13,6 +17,16 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Routes>
+            <Route
+              path='/'
+              element={<Home />}
+            />
+          </Routes>
+        </div>
+      </Router>
       
     </ApolloProvider>
   );
---
+++ client/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <main>
+            <div>
+                <h1>Hi there</h1>
+            </div>
+        </main>
+    )
+}
+
+export default Home;
