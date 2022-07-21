2e52a0f9b1345630c6824f7700d992b680bf9903
Beimy <arbm159357@gmail.com>
2022-07-21 17:42:38 -0400

    home page is now able to fetch for specific recipies

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,38 +1,53 @@
 import React, { useState, useEffect } from "react";
 
+const app_id = process.env.REACT_APP_EDAMAM_APP_ID;
+const api_key = process.env.REACT_APP_EDAMAM_API_KEY;
+
 const Home = () => {
     const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(null);
     const [error, setError] = useState(null);
+    const [searchQuery, setSearchQuery] = useState(null);
 
-    useEffect(() => {
+    function sendReq() {
+        setLoading(true);
+
         const getData = async () => {
-          try {
-            const response = await fetch(
-                `https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=66de0d20&app_key=%20c50d2eae3c4c8260427bcddef5eb242f%09`
-            );
-            if (!response.ok) {
-                throw new Error(
-                    `This is an HTTP error: Status ${response.status}`
-                );
+            try {
+              const response = await fetch(
+                  `https://api.edamam.com/api/recipes/v2?type=public&q=${searchQuery}&app_id=${app_id}&app_key=${api_key}`
+              );
+              if (!response.ok) {
+                  throw new Error(
+                      `This is an HTTP error: Status ${response.status}`
+                  );
+              }
+              let data = await response.json();
+              setData(data);
+              setError(null);
+            } catch(err) {
+              setError(err.message);
+              setData(null);
+            } finally {
+              setLoading(false);
             }
-            let data = await response.json();
-            setData(data);
-            setError(null);
-          } catch(err) {
-            setError(err.message);
-            setData(null);
-          } finally {
-            setLoading(false);
           }
-        }
-        getData()
-    }, [])
+          getData()
+    }
 
     return (
         <main>
             <div>
                 <h1>Hi there</h1>
+                {loading && <div>A moment...</div>}
+                <form>
+                    <label>Enter Food to search recipes by:
+                        <input value={searchQuery} onInput={e => setSearchQuery(e.target.value)}/>
+                    </label>
+                    <button type="button" onClick={sendReq}>Send Request</button>
+                </form>
+                
+                
             </div>
         </main>
     )
