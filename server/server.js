8d1182254388f178880e5216cbeea56042578e72
Skylar Jackson <skylarjackson@Skylars-Air.attlocal.net>
2022-07-23 13:05:17 -0400

    small fix to get server runnning properly

--- server/server.js
+++ server/server.js
@@ -27,14 +27,6 @@ const startApolloServer = async (typeDefs, resolvers) => {
   server.applyMiddleware({ app });
 }
 
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
