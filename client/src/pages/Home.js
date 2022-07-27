c76abd3e65cfd198ae437f2f9f3890f69369d46e
Vshepard879 <vshepard879@gmail.com>
2022-07-26 23:50:29 -0400

    added Jamie's super sweet icons

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,30 +4,14 @@ import SignUpModal from "../components/SignUpModal";
 import { Outlet } from "react-router-dom";
 import HomePage from '../components/homePage/homePage.component'
 const Home = () => {
-    const [isLoginModalActive, setIsLoginModalActive] = useState(false);
-    const [isSignUpModalActive, setisSignUpModalActive] = useState(false);
+   
 
     return (
         <div>
-        <Fragment>
-            <button type="button" onClick={function() {setIsLoginModalActive(true)}}>Login</button>
-            {isLoginModalActive && <LoginModal
-                                modalToggle={setIsLoginModalActive}
-                                
-                                />
-            }
-
-            <button type="button" onClick={function() {setisSignUpModalActive(true)}}>Sign Up</button>
-            {isSignUpModalActive && <SignUpModal
-                                modalToggle={setisSignUpModalActive}
-                                
-                                />
-            }
-        </Fragment>
-        <div>
+       
                     <Outlet />
                     <HomePage/>
-                    </div>
+                    
         </div>
     )
     
