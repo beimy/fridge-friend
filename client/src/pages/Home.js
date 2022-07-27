7a6db0fba1f34106d2cb6ead3e67fa213c99eda7
Vshepard879 <vshepard879@gmail.com>
2022-07-27 01:41:48 -0400

    Started layout for UserProfile

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,5 @@
 import React, { useEffect, Fragment, useState } from "react";
-import LoginModal from "../components/LoginModal";
-import SignUpModal from "../components/SignUpModal";
+
 import { Outlet } from "react-router-dom";
 import HomePage from '../components/homePage/homePage.component'
 const Home = () => {
