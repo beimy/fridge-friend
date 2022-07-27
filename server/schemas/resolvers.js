a16fb5737bb835e86bfc918381ca30a703cf2ae4
Martin C <98830462+Devnah4@users.noreply.github.com>
2022-07-26 22:53:23 -0400

    Commented out test deletion code.
    No longer saves favorited food. uncommenting current code will break the system.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -84,7 +84,7 @@ const resolvers = {
 
         const userData = await User.findByIdAndUpdate(
           { _id: context.user._id },
-          { $push: { favRecipes: recipe } },
+          { $push: { favRecipes: { _id: args._id } } },
           { new: true }
         );
 
@@ -94,20 +94,20 @@ const resolvers = {
 
       throw new AuthenticationError("You need to be logged in!");
     },
-    removeRecipe: async (parent, args, context) => {
-      if (context.user) {
-        const updatedUser = await User.findOneAndUpdate(
-          { _id: context.user._id },
+    // removeRecipe: async (parent, args, context) => {
+    //   if (context.user) {
+    //     const updatedUser = await User.findOneAndUpdate(
+    //       { _id: context.user._id },
 
-          { $pull: { favRecipes: { name: args.name } } },
+    //       { $pull: { favRecipes: recipe } },
 
-          { new: true }
-        );
-        console.log(updatedUser);
-        return updatedUser;
-      }
-      throw new AuthenticationError("Please login in!");
-    },
+    //       { new: true }
+    //     );
+    //     console.log(updatedUser);
+    //     return updatedUser;
+    //   }
+    //   throw new AuthenticationError("Please login in!");
+    // },
   },
 };
 
