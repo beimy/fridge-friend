6640080b9e6be258698a2ef0564948a98f6d6848
Vshepard879 <vshepard879@gmail.com>
2022-07-27 21:17:54 -0400

    stuff

--- client/src/pages/UserProfilePage.js
+++ client/src/pages/UserProfilePage.js
@@ -1,83 +1,52 @@
-import React from 'react';
-import { Redirect, useParams } from 'react-router-dom';
-import { QUERY_USER, QUERY_ME } from '../utils/queries';
-import ThoughtList from '../components/ThoughtList';
-import FriendList from '../components/FriendList';
-import { useQuery, useMutation } from '@apollo/client'
-import Auth from '../utils/auth';
-import { ADD_FRIEND } from '../utils/mutations';
-import ThoughtForm from '../components/ThoughtForm';
+import React, { Component } from 'react';
+import { QUERY_RECIPES, QUERY_ME } from '../utils/queries';
+import { useQuery } from '@apollo/client';
+import Recipe from '../components/Recipe';
+// import UserProfile from 'react-user-profile';
 
+ 
+const UserProfilePage = () => {
 
-const Profile = () => {
+  const {loading, error, data} = useQuery(QUERY_ME);
 
-  const [addFriend] = useMutation(ADD_FRIEND);
+  // const photo = '/'
+  // const userName = 'Harvey Specter'
+  // const location = 'New York, USA'
 
-  const { username: userParam } = useParams();
+  // const comments = [
+  //   {
+  //     id: '1',
+  //     photo: '/',
+  //     userName: 'Mike Ross',
+  //     content: 'Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies. Curabitur et ligula. ',
+  //     createdAt: 1543858000000
+  //   }
+  // ];
 
-  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
-    variables: { username: userParam }
-  });
-
-  const user = data?.me || data?.user || {};
-
-  // redirect to personal profile page if username is the logged-in user's
-  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
-    return <Redirect to="/profile" />;
-  }
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (!user?.username) {
+    if(loading) return 'Loading';
+    if(error) return `Error! ${error.message}`;
+    console.log(data.me.favRecipes);
+ 
     return (
-      <h4>
-        You need to be logged in to see this page. Use the navigation links above to sign up or log in!
-      </h4>
-    );
-  }
+      <div style={{ margin: '0 auto', width: '100%' }}>
+        <ul>
+          {data.me.favRecipes.map(recipe => (
+            <li key={recipe.uri}>
+                <Recipe
+                key={recipe.uri} 
+                title={recipe.title}
+                image={recipe.image} 
+                ingredients={recipe.ingredientLines}
+                uri={(recipe.uri).split('')[1]}
+                url={recipe.url}
+                />
+            </li>
+          ))}
+        </ul>
 
-  const handleClick = async () => {
-    try {
-      await addFriend({
-        variables: { id: user._id }
-      });
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
-  return (
-    <div>
-      <div className="flex-row mb-3">
-        <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
-        </h2>
-
-        {userParam && (
-          <button className="btn ml-auto" onClick={handleClick}>
-            Add Friend
-          </button>
-        )}
       </div>
+    )
 
-      <div className="flex-row justify-space-between mb-3">
-        <div className="col-12 mb-3 col-lg-8">
-          <ThoughtList thoughts={user.thoughts} title={`${user.username}'s thoughts...`} />
-        </div>
-
-        <div className="col-12 col-lg-3 mb-3">
-          <FriendList
-            username={user.username}
-            friendCount={user.friendCount}
-            friends={user.friends}
-          />
-        </div>
-      </div>
-      <div className="mb-3">{!userParam && <ThoughtForm />}</div>
-    </div>
-  );
-};
-
-export default Profile;
+}
+ 
+export default UserProfilePage;
